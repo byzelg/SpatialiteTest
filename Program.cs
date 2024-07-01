b82243903659b8423ae033a16b741a37e348b0f6
byzelg <beyzanurelgun@gmail.com>
2024-07-01 14:52:03 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Data.SQLite;
+
+
+namespace SpatialiteTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Data Source=template.sqlite;Version=3;";
+
+            while (true)    //Çıkış yapılmadığı takdirde seçimin tekrar yapılabilmesi için.
+            {
+                Console.WriteLine("Lütfen bir seçenek seçiniz:");
+                Console.WriteLine("1. Bina geometrik objeleri oluştur ve kaydet");
+                Console.WriteLine("2. Kapıların, binanın içinde mi dışında mı olduğunu belirle ve güncelle");
+                Console.WriteLine("3. Kapı sonuçlarını görüntüle");
+                Console.WriteLine("4. Çıkış");
+                string secim = Console.ReadLine();
+
+                switch (secim)
+                {
+                    case "1":
+                        CreateAndSave(connectionString);
+                        break;
+                    case "2":
+                        DoorInOut(connectionString);
+                        break;
+                    case "3":
+                        DoorResult(connectionString);
+                        break;
+                    case "4":
+                        Console.WriteLine("Programdan çıkılıyor...");
+                        return;
+                    default:
+                        Console.WriteLine("Geçersiz seçim. Lütfen tekrar deneyin...");
+                        break;
+                }
+            }
+        }
+        static void CreateAndSave(string connectionString)
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+                connection.LoadExtension("mod_spatialite");
+                var command = connection.CreateCommand();
+                command.CommandText = @"SELECT building_id, node_order, st_x(geom) as x, st_y(geom) as y FROM building_nodes";
+                using (var reader = command.ExecuteReader())
+                {
+                    string polygonText = "POLYGON(";
+                    string pointList = "";
+                    string temp = "";
+                    while (reader.Read())
+                    {
+                        var buildingId = reader.GetValue(0);
+                        var nodeOrder = reader.GetValue(1);
+                        double x = reader.GetDouble(2);
+                        double y = reader.GetDouble(3);
+                       
+
+                        //POLYGON(10 10, 20 20, 30 30, 10 10)
+                        if (temp == "")
+                        {
+                            temp = buildingId.ToString();
+                        }
+                        if(temp != buildingId.ToString())
+                        {
+                            polygonText += $"({pointList + pointList.Split(',')[0]}))";
+                            using (var insertCmd = new SQLiteCommand("REPLACE INTO building (id, geom) VALUES (@buildingId, GeomFromText(@polygonText,4326))", connection))
+                            {
+                                insertCmd.Parameters.AddWithValue("@polygonText", polygonText);
+                                insertCmd.Parameters.AddWithValue("@buildingId", temp);
+                                insertCmd.ExecuteNonQuery();
+                            }
+                            temp = buildingId.ToString();
+                            polygonText = "POLYGON(";
+                            pointList = "";
+                        }
+                        pointList += $"{x.ToString().Replace(',', '.')} {y.ToString().Replace(',', '.')},";
+                    }
+                    
+                }
+            }
+            Console.WriteLine("Building geometrik objeleri oluşturuldu ve kaydedildi.");
+        }
+
+
+        static void DoorInOut(string connectionString)
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+                connection.LoadExtension("mod_spatialite");
+                string sql = @"UPDATE door
+                                    SET inside_building = 
+                                    CASE
+                                        WHEN b.id IS NOT NULL AND ST_Within(d.geom, b.geom) THEN 1
+                                        WHEN b.id IS NOT NULL AND NOT ST_Within(d.geom, b.geom) THEN 0
+                                        ELSE NULL
+                                    END
+                            FROM door d
+                            LEFT JOIN building b ON d.building_id = b.id;";
+
+                /*CREATE TEMPORARY TABLE temp AS
+                  SELECT d.*, 
+                    CASE
+                        WHEN b.id IS NOT NULL AND ST_Within(d.geom, b.geom) THEN 1
+                        WHEN b.id IS NOT NULL AND NOT ST_Within(d.geom, b.geom) THEN 0
+                        ELSE NULL
+                    END AS inside_building
+                  FROM door d
+                  LEFT JOIN building b ON d.building_id = b.id;
+
+                  UPDATE door
+                  SET inside_building = (SELECT inside_building FROM temp WHERE door.id = temp.id);
+
+                  DROP TABLE temp;*/
+                using (SQLiteCommand command = new SQLiteCommand(sql, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+            }
+            Console.WriteLine("Kapıların içinde mi dışında mı olduğu belirlendi ve güncellendi.");
+        }
+
+
+        static void DoorResult(string connectionString)
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+                connection.LoadExtension("mod_spatialite");
+                var command = connection.CreateCommand();
+                command.CommandText = @"select id, building_id, door_no, inside_building from door";
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var doorId = reader.GetValue(0);
+                        var buildingId = reader.GetValue(1);
+                        var doorNo = reader.GetValue(2);
+                        var inside = reader.GetValue(3);
+                        switch (inside)
+                        {
+                            case 0:
+                                inside = "Hayır";
+                                break;
+                            case 1:
+                                inside = "Evet";
+                                break;
+                            default:
+                                inside = "Tespit Edilemedi";
+                                break;
+                        }
+                        Console.WriteLine($"Kapi id: {doorId}, Bina id: {buildingId}, Kapi No: {doorNo}, Bina İçinde Mi: {inside}");
+                    }
+                }
+            }
+        }
+    }
+}
+//Console.ReadLine();
---
+++ ReadMe.txt
@@ -0,0 +1,47 @@
+﻿Notes:
+ Nuget Packages;
+ + Nuget System.Data.SQLite package install:  NuGet\Install-Package System.Data.SQLite -Version 1.0.118
+ + Nuget mode_spatialilte package install: NuGet\Install-Package mod_spatialite -Version 4.3.0.1 
+ 
+ Spatialite:
+	+ Spatialite Sqlite dosya bazlı veri tabanı motorunun konumsal veri saklama ve soruglama yeteneklerinin eklenmiş halidir. 
+	+ Spatialite dosyalarında sözel veriler ile birlikte geometrik özellikli(nokta, çizgi, alan) verileri ilişkisel tablolarda saklayabilir ve bu verileri üzerinden çeşitli 
+		coğrafi sorgular(kesişim, en yakın mesafe, Koordinat vb.) yapılabilir. 
+		 
+
+ Referans linkler;
+ + https://dominoc925.blogspot.com/2016/10/simple-c-example-for-creating-and-using.html
+ + https://www.gaia-gis.it/gaia-sins/spatialite-sql-4.2.0.html
+ + https://www.gaia-gis.it/gaia-sins/spatialite-cookbook-5/index.html
+
+ QGIS:
+ + template.sqlite dosyası içindeki verileri görsel olarak görmek için kullanılabilecek açık kaynak kodlu GIS yazılımı.
+ + https://www.qgis.org/en/site/
+ + https://www.youtube.com/watch?v=gC0z4miZmtQ
+
+ template.sqlite: 
+    + Dosya içerisinde, building, building_nodes, door isminde 3 adet tablo bulunmaktadır.
+	+ Tablolar arasındaki ilişki spatialtest.jpg dosyasında mevcuttur. 
+
+ Program Çalışma şekli;
+ + Program komut satırı uygulaması olacak
+ + Kullanıcı programı çalıştırdığında, karşısına 3 adet seçenek çıkacak,
+ + Kullanıcı bu 3 adet seçeneği klaveyden komut girerek istediği sıra ile çalıştırabilecek, 
+
+
+ + 1. Seçenek de template veri tabanındaki building_nodes tablosundaki kayıtlar okunarak, building_id ve node_order kolonlarına göre bina geometrik objeleri oluşturulacak
+	ve building tablosuna kayıt edilecek, ilgili bina, tabloda zaten mevcutsa silinip tekrar eklenecek 
+
+
+
+
+ + 2. Seçenek de door tablosu okunarak her bir kapının konumu, building_id'ye göre building tablosu ile karşılaştırılarak, ilgili kapının binanın içinde mi yoksa dışında mı
+   olduğu spatialite kütüphanesi konumsal analiz fonksiyonları kullanılarak tespit edilecek ve sonuçlar door tablosunda inside_building kolonuna, içinde için "1", dışında için "0" olacak şekilde update edilecek. 
+   Eğer ilgili kapi için building tablosunda kayıt yoksa inside_building kolonu null olarak update edilecek.
+
+
+
+ + 3. Seçenek de door sonuçları aşağıdaki örnek şeklinde ekrana yazdırılacak;
+	* Kapı id: 1, Bina id: 2, Kapı No: 2A, Bina İçinde Mi: Evet --inside_building kolonu 1 olan kayıt
+	* Kapi id: 2, Bina id: 3, Kapı No: 16, Bina İçinde Mi: Hayır --inside_building kolonu 0 olan kayıt
+	* Kapi id: 3, Bina id: 1, Kapı No: 12, Bina İçinde Mi: Tespit Edilemedi --inside_building kolonun null olan kayıt
